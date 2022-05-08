4931e8bee1d94937a9c981b4ce497e748a2ac63e
daviddflix <david-972010@hotmail.com>
2022-05-08 05:16:07 -0300

    fixing controller compras value

--- src/controllers/compras.js
+++ src/controllers/compras.js
@@ -1,42 +1,34 @@
 const {Compras} = require('../database')
 
-// toppings_finals: [],
-// salsa: [],
-// priceTopping: null,
-// id: '',
-// product: '',
-// price: 0,
-// picture: '',
-// Comments: '',
-// finalPrice: 0
+// {finalPrice, 
+//   Comments, 
+//   picture, 
+//   price, 
+//   product,
+//   id,
+//   priceTopping, salsa, toppings_finals}
+
+// Compras.create({
+//   image: picture,
+//   pastas: product,
+//   price: finalPrice,
+//   salsas: salsa,
+//   comments: Comments,
+//   toppings: toppings_finals
+// })
 
  const compras = (async (req, res) => {
-      const {finalPrice, 
-        Comments, 
-        picture, 
-        price, 
-        product,
-        id,
-        priceTopping, salsa, toppings_finals} = req.body || req.query
-      console.log('data', finalPrice)
+      const  value = req.body || req.query
+     
 
     try {
+
         
-        const compra = await  Compras.findAll();// data de la tabla
-        console.log('compra',compra)
-        
-       if(finalPrice){
-        Compras.create({
-            image: picture,
-            pastas: product,
-            price: finalPrice,
-            salsas: salsa,
-            comments: Comments,
-            toppings: toppings_finals
-        })
+       if(value){
+         console.log('value',value)
         res.send('purchase added')
        } else{
-        res.send('purchase not added to the DB, PRICE not provided')
+        res.send('purchase not added to the DB, value not provided')
        }    
         
     } catch (error) {
