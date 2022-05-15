91aa3b4f0cbd11c3096ed5dce2209ee80752f0ce
daviddflix <david-972010@hotmail.com>
2022-05-15 08:24:54 -0300

    back to headers

--- src/app.js
+++ src/app.js
@@ -6,20 +6,20 @@ const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
 
 app.use(express.json())
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
-app.use(bodyParser.json({ limit: '50mb' }))
-app.use(express.json({ extended: true }))
-app.use(express.urlencoded({ extended: true }))
+// app.use(cors())
+// app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
+// app.use(bodyParser.json({ limit: '50mb' }))
+// app.use(express.json({ extended: true }))
+// app.use(express.urlencoded({ extended: true }))
 
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-//     res.header('Access-Control-Allow-Credentials', 'true');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//     next();
-//   });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
   // app.use(express.urlencoded({extended: false}))
 
 app.use(express.static('public'))
