ec7076e2a6d41d80fea35445b3c732a2948b97d4
daviddflix <david-972010@hotmail.com>
2022-05-15 07:38:19 -0300

    installing bodyparser

--- src/app.js
+++ src/app.js
@@ -3,10 +3,14 @@ const app = express()
 const path = require('path')
 const cors = require('cors')
 const favicon = require('serve-favicon')
-
+const bodyParser = require('body-parser')
 
 app.use(express.json())
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(express.json({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 
 
 // app.use((req, res, next) => {
