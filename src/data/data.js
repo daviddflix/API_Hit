d2c9f8c35bacef4f1d67addf0f179e4fd1d571d0
daviddflix <david-972010@hotmail.com>
2022-05-20 17:48:20 -0300

    adding images

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 const product = ['Penne Rigate', 'Conchiglie', 'Cavatelli', 'Gnocchi', 'Spaguetti', 'Tagliattele' ]
 const sauces = ['Pomodoro', 'Crema', 'Arrabiata', 'Bolognesa', 'Pesto', 'Cheddar', 'Puttanesca', 'Oriental', 'Curry', 'Cuatro quesos', 'Crema de espinaca', 'Vodka sauce']
 const extra = ['Meatball','Bacon', 'Carne', 'Pollo', 'Queso Parmesano', 'Hongos', 'Aceitunas', 'Jalapeños', 'Tomates Confitados', 'Alcaparras', 'Panes']
-const dessert = ['Cheesecake', 'Chocotorta', 'Tiramisu']
+const dessert = [{product: 'Cheesecake', picture: '/img/Images/cheesecake.jpg'}, {product: 'Chocotorta', picture: '/img/Images/chocotorta.jpg'}, {product: 'Tiramisu', picture: 'img/Imges/tiramisu.png'}]
 const beers = [{product: 'Imperial Lager 473ml', price: 350}, {product: 'Heineken Regular 473ml', price: 420}, {product: 'Imperial Amber Lager 473ml', price: 350}, {product: 'Imperial Ipa 473ml', price: 350}]
 const beverages = [{product: 'Coca-Cola Light 354ml', price: 230}, {product: 'Coca-Cola Sin Azúcar', price: 270}, {product: 'Coca-Cola Sabor Original 354ml', price: 270}, {product: 'Fanta Naranja 354ml', price: 270}, {product: 'Sprite Lima Limón 354ml', price: 270}, {product: 'Sprite Lima Limón 354ml', price: 270}, {product: 'Coca-Cola Light 354ml', price: 230}, {product: 'SmartWater Sin Gas 591ml', price: 270}]
 const dessertPrice = 400
