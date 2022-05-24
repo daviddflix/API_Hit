482eea3dc6faaf826f453ac9ea83650a8f1adc7c
daviddflix <david-972010@hotmail.com>
2022-05-24 11:37:16 -0300

    getting webhook

--- src/controllers/notification.js
+++ src/controllers/notification.js
@@ -1,42 +1,60 @@
 const {Pagos} = require('../database')
 
-var mercadopago = require('mercadopago');
-mercadopago.configurations.setAccessToken(process.env.TEST_ACCESS_TOKEN);
+// var mercadopago = require('mercadopago');
+// mercadopago.configurations.setAccessToken(process.env.TEST_ACCESS_TOKEN);
 
 const notification = (async(req, res) => {
+
     console.log('body:',req.body)
-    // var payment_data = {
-    //     transaction_amount: Number(req.body.transactionAmount),
-    //     token: req.body.token,
-    //     description: req.body.description,
-    //     installments: Number(req.body.installments),
-    //     payment_method_id: req.body.paymentMethodId,
-    //     issuer_id: req.body.issuer,
-    //     notification_url: "http://requestbin.fullcontact.com/1ogudgk1",
-    //     payer: {
-    //       email: req.body.email,
-    //       identification: {
-    //         type: req.body.docType,
-    //         number: req.body.docNumber
-    //       }
-    //     }
-    //   };
-      
-    //   mercadopago.payment.save(payment_data)
-    //     .then(function(response) {
-    //       res.status(response.status).json({
-    //         status: response.body.status,
-    //         status_detail: response.body.status_detail,
-    //         id: response.body.id
-    //       });
-    //     })
-        // .catch(function(error) {
-        //   res.status(response.status).send(error);
-        // });
-
-    // https://api.mercadopago.com/v1/payments/[ID]
-
     res.sendStatus(200)
+
+   const url = `https://api.mercadopago.com/v1/payments/${req.body.id}`
+
+   const payment = await axios.get(url, {
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${process.env.TEST_ACCESS_TOKEN}`
+                }
+              });
+
+              console.log('datosPayment:', payment)
+
 })
 
 module.exports = notification
+
+
+
+
+
+
+
+// var payment_data = {
+//     transaction_amount: totalAmount,
+//     // token: req.body.token,
+//     // description: req.body.description,
+//     // installments: Number(req.body.installments),
+//     // payment_method_id: req.body.paymentMethodId,
+//     // issuer_id: req.body.issuer,
+//     notification_url: "https://hit-pasta.herokuapp.com/postnotification",
+//     payer: {
+//       email: items.user.email,
+//     //   identification: {
+//     //     type: req.body.docType,
+//     //     number: req.body.docNumber
+//     //   }
+//     }
+//   };
+  
+//   mercadopago.payment.save(payment_data)
+//     .then(function(response) {
+//         console.log('notification: ', response)
+//       res.status(response.status).json({
+//         status: response.body.status,
+//         status_detail: response.body.status_detail,
+//         id: response.body.id
+//       });
+//     })
+//     .catch(function(error) {
+//       console.log('errornotification: ', error)
+//     });
