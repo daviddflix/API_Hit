e1c2bc6e6d57356d4a2c2ee7ae60eff91f142a88
daviddflix <david-972010@hotmail.com>
2022-05-28 15:31:51 -0300

    get user with pagos and comr

--- src/controllers/getUser.js
+++ src/controllers/getUser.js
@@ -9,25 +9,39 @@ const {User, Compras, Pagos} = require('../database')
         
       const  users = await User.findAll()
      
-        if(users.length && phonenumber || email || name){
+        if(users.length){
+          if(phonenumber || email || name){
+
             const UserByPhone = await  User.findAll({where: {phonenumber : {[Op.iLike]: '%'+ phonenumber + '%'}}, include: [Compras, Pagos]});
             const UserByEmail = await  User.findAll({where: {phonenumber : {[Op.iLike]: '%'+ email + '%'}}, include: [Compras, Pagos]});
             const UserByName = await  User.findAll({where: {phonenumber : {[Op.iLike]: '%'+ name + '%'}}, include: [Compras, Pagos]});
 
             if(UserByPhone){
               res.send(UserByPhone)
+            } else{
+              res.send('user not found')
             }
 
             if(UserByEmail){
               res.send(UserByEmail)
+            } else{
+              res.send('user not found')
             }
+
             if(UserByName){
               res.send(UserByName)
+            } else{
+              res.send('user not found')
             }
         
+
+          }else{
+            res.send(users)  
+        }
+            
           
         } else{
-            res.send(users)  
+            res.send('DB empty')  
         }
        
        
