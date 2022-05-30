9d77912103da5dcbf9abe4d5c679c40e8f3b7339
daviddflix <david-972010@hotmail.com>
2022-05-30 11:04:47 -0300

    swal 3

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express()
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin : "https://hitpasta.vercel.app",
+    origin : "*",
     credentials: true,
   }
 })
