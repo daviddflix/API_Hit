bb524030a74175d846f14dc0808417c631d8712a
daviddflix <david-972010@hotmail.com>
2022-07-12 16:53:42 -0300

    changing route

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,24 +1,22 @@
 const data = require('../data/data')
-const {Products} = require('../database')
+const { Products } = require('../database')
 
 
  const products = (async (req, res) => {
     
     try { 
-        
-        const info = await  Products.findAll();// data de la tabla
+        const info = await Products.findAll();// data de la tabla
        
         if(info.length === 0){
-            Products.bulkCreate(data) // llena la Db
+          await Products.bulkCreate(data) // llena la tabla
           }
-          if(info.length){
+
+        if(info.length > 1){
             res.send(info)
         }
     } catch (error) {
         console.log('algo paso',error)
     }
-
-  
 })
 
 module.exports = products
